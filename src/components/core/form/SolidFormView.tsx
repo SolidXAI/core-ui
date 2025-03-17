d7d0dab636a188e321594323058149069e511549
Jenendar <jenendar.logicloop@gmail.com>
2025-03-17 17:57:18 +0530

    bigint added

--- src/components/core/form/SolidFormView.tsx
+++ src/components/core/form/SolidFormView.tsx
@@ -89,7 +89,7 @@ const fieldFactory = (type: string, fieldContext: SolidFieldProps): ISolidField 
     if (type === 'longText') {
         return new SolidLongTextField(fieldContext);
     }
-    if (type === 'int') {
+    if (type === 'int' || type === 'bigint' ) {
         return new SolidIntegerField(fieldContext);
     }
     if (type === 'decimal' || type === 'float') {
@@ -927,7 +927,6 @@ const SolidFormView = (params: SolidFormViewProps) => {
                                     {params.embeded !== true &&
                                         actionsAllowed.includes(`${createPermission(params.modelName)}`) &&
                                         !formViewLayout.attrs.readonly &&
-                                        formik.dirty &&
                                         <div>
                                             <Button
                                                 label="Save"
