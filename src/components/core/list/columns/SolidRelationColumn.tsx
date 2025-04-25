2e384d25c8b90aaaa8e76ad119e7eab5d671bdcf
Jenendar <jenendar.logicloop@gmail.com>
2025-04-25 17:54:02 +0530

    bug fix

--- src/components/core/list/columns/SolidRelationColumn.tsx
+++ src/components/core/list/columns/SolidRelationColumn.tsx
@@ -2,13 +2,14 @@
 import { SolidListViewColumnParams } from '../SolidListViewColumn';
 import SolidRelationManyToManyColumn from './relations/SolidRelationManyToManyColumn';
 import SolidRelationManyToOneColumn from './relations/SolidRelationManyToOneColumn';
+import SolidRelationOneToManyColumn from './relations/SolidRelationOneToManyColumn';
 
 const SolidRelationColumn = ({ solidListViewMetaData, fieldMetadata, column }: SolidListViewColumnParams) => {
     if (fieldMetadata.relationType === 'many-to-one') {
         return SolidRelationManyToOneColumn({ solidListViewMetaData, fieldMetadata, column });
     }
     if (fieldMetadata.relationType === 'one-to-many') {
-        return SolidRelationManyToOneColumn({ solidListViewMetaData, fieldMetadata, column });
+        return SolidRelationOneToManyColumn({ solidListViewMetaData, fieldMetadata, column });
     }
     if (fieldMetadata.relationType === 'many-to-many') {
         return SolidRelationManyToManyColumn({ solidListViewMetaData, fieldMetadata, column });
