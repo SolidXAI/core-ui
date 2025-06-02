d0a26e0895e05595b47e21cddd453afa702098a9
Jenendar <jenendar.logicloop@gmail.com>
2025-06-02 08:39:26 +0530

    changes

--- src/components/core/kanban/kanban-fields/SolidDateKanbanField.tsx
+++ src/components/core/kanban/kanban-fields/SolidDateKanbanField.tsx
@@ -5,7 +5,7 @@ import { SolidKanbanViewFieldsParams } from '../SolidKanbanViewFields';
 const SolidDateKanbanField = ({ solidKanbanViewMetaData, fieldMetadata, fieldLayout,data }: SolidKanbanViewFieldsParams) => {
     
     return (
-        <p className='m-0'>{data && data[fieldMetadata.name] ? `${data[fieldMetadata.name]}`: ""}</p>
+        <p className='m-0'>{fieldLayout?.attrs?.showLabel !== false && fieldLayout?.attrs?.label ? `${fieldLayout?.attrs?.label} : ` : ""}{data && data[fieldMetadata.name] ? `${data[fieldMetadata.name]}`: ""}</p>
 
     );
 
