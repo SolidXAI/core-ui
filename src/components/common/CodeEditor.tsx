828db6e2b4f4c6dea2da012d85d503b78c438995
Jenendar <jenendar.logicloop@gmail.com>
2025-09-26 19:31:18 +0530

    changes

--- src/components/common/CodeEditor.tsx
+++ src/components/common/CodeEditor.tsx
@@ -5,11 +5,23 @@ import { oneDark } from '@codemirror/theme-one-dark';
 import CodeMirror, { EditorView } from '@uiw/react-codemirror'; // Correct import
 
 const CodeEditor = ({ formik, field, height, fontSize, readOnly }: any) => {
+  let code;
+  const value = formik?.values[field];
+
+  if (value == null) {
+    // null or undefined → empty string
+    code = "";
+  } else if (typeof value === "string") {
+    code = value;
+  } else {
+    code = JSON.stringify(value, null, 2);
+  }
+
 
   return (
     <CodeMirror
       id={field}
-      value={formik?.values[field]}
+      value={code}
       height={height ?? '300px'}
       style={{ fontSize: fontSize ?? '10px' }}
       theme={oneDark}
