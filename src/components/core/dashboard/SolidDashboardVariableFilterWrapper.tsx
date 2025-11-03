eb808f95728b93e8db2a8c06a5e672c2e53a5127
Chetan <chetan.koli@logicloop.io>
2025-11-03 12:01:10 +0530

    autocomplete fields

--- src/components/core/dashboard/SolidDashboardVariableFilterWrapper.tsx
+++ src/components/core/dashboard/SolidDashboardVariableFilterWrapper.tsx
@@ -9,6 +9,7 @@ import { Nullable } from "primereact/ts-helpers";
 import { SqlExpression } from "@/types/solid-core";
 import styles from './SolidDashboard.module.css'
 import { Tooltip } from "primereact/tooltip";
+import { AutoComplete, AutoCompleteChangeEvent, AutoCompleteCompleteEvent } from "primereact/autocomplete";
 export interface SolidDashboardVariablesFilterDialogProps {
   dashboardVariableFilterRules: ISolidDashboardVariableFilterRule[];
   setDashboardVariableFilterRules: Dispatch<SetStateAction<ISolidDashboardVariableFilterRule[]>>;
@@ -20,6 +21,35 @@ const SolidDashboardVariableFilterDialog: React.FC<SolidDashboardVariablesFilter
 
   const [showFilterDialog, setShowFilterDialog] = useState(false);
   const [dates, setDates] = useState<Nullable<(Date | null)[]>>(null);
+
+  // Selection Static Values
+  const [selectionStaticValues, setSelectionStaticValues] = useState<string[]>([]);
+  const [filteredStaticItems, setFilteredStaticItems] = useState<string[]>([]);
+  const staticItems = ["Value 1", "Value 2", "Value 3"];
+
+  // Selection Dynamic Values
+  const [selectionDynamicValues, setSelectionDynamicValues] = useState<string[]>([]);
+  const [filteredItems, setFilteredItems] = useState<string[]>([]);
+  const allItems = ["Value1", "Value2", "Value3"];
+
+  // Static search
+  const searchStatic = (event: AutoCompleteCompleteEvent) => {
+    const query = event.query.toLowerCase();
+    const filtered = staticItems.filter(item =>
+      item.toLowerCase().includes(query)
+    );
+    setFilteredStaticItems(filtered);
+  };
+
+  // Dynamic search
+  const searchDynamic = (event: AutoCompleteCompleteEvent) => {
+    const query = event.query.toLowerCase();
+    const filtered = allItems.filter(item =>
+      item.toLowerCase().includes(query)
+    );
+    setFilteredItems(filtered);
+  };
+
 
   // TODO [HP]: Currently this is static, we need this to be dynamic how we are invoking setFilters below has to be fully dynamic...
   return (
@@ -36,7 +66,7 @@ const SolidDashboardVariableFilterDialog: React.FC<SolidDashboardVariablesFilter
           </>
         }
       </p>
-      <div>
+      <div className="flex align-items-center gap-3">
         <div className={`flex align-items-center ${styles.SolidDashboardDateRangeFilterWrapper}`}>
           <Calendar value={dates} onChange={(e) => {
             console.log(`Calendar changed values are: `);
@@ -61,11 +91,36 @@ const SolidDashboardVariableFilterDialog: React.FC<SolidDashboardVariablesFilter
               });
             }
 
-          }} selectionMode="range" readOnlyInput hideOnRangeSelection inputClassName="h-2rem" className={styles.SolidDashboardDateRangeFilter} />
+          }} selectionMode="range" readOnlyInput hideOnRangeSelection inputStyle={{height: 36.38}} className={styles.SolidDashboardDateRangeFilter} />
           <div className="px-2">
             <i className="pi pi-calendar opacity-50"></i>
           </div>
         </div>
+         {/* Static Selection */}
+      <AutoComplete
+        value={selectionStaticValues}
+        suggestions={filteredStaticItems}
+        completeMethod={searchStatic}
+        onChange={(e: AutoCompleteChangeEvent) => setSelectionStaticValues(e.value)}
+        multiple
+        dropdown
+        placeholder="Selection Static Field"
+        className="solid-standard-autocomplete"
+        style={{minHeight: 38}}
+      />
+
+      {/* Dynamic Selection */}
+      <AutoComplete
+        value={selectionDynamicValues}
+        suggestions={filteredItems}
+        completeMethod={searchDynamic}
+        onChange={(e: AutoCompleteChangeEvent) => setSelectionDynamicValues(e.value)}
+        multiple
+        dropdown
+        placeholder="Selection Dynamic Field"
+        className="solid-standard-autocomplete"
+        style={{minHeight: 38}}
+      />
         {/* <Button size="small" label="Filter" outlined onClick={()=>setShowFilterDialog(true)}/> */}
       </div>
 
