65b4a2b2bcc29bea46a5ead0f86ef4ed2fced259
Jenendar <jenendar.logicloop@gmail.com>
2025-12-17 16:38:00 +0530

    bug fix

--- src/components/core/form/SolidFormViewNormalHeaderButton.tsx
+++ src/components/core/form/SolidFormViewNormalHeaderButton.tsx
@@ -24,7 +24,7 @@ export function SolidFormViewNormalHeaderButton({
         : useHasAnyRole(button?.attrs?.roles);
 
     if (!hasRole) return null;
-    if (button?.attrs?.visible == "false") return null
+    if (button?.attrs?.visible == false) return null
     return (
         <div>
         <Button
