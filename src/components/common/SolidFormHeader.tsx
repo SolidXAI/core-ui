0562ef0230f814571516bfbdcc95a14387ea1f5f
Harish Patel <harish@logicloop.io>
2025-12-29 10:41:53 +0000

    changes around ai interaction ui and displaying workflow field on create

--- src/components/common/SolidFormHeader.tsx
+++ src/components/common/SolidFormHeader.tsx
@@ -16,7 +16,7 @@ export const SolidFormHeader = (props: Props) => {
     return (
         <div className='flex flex-column gap-2 align-items-start xl:flex-row xl:align-items-center justify-content-between solid-dynamic-breadcrumb-stepper'>
             <SolidBreadcrumb {...props} />
-            {solidFormViewMetaData?.data?.solidFormViewWorkflowData.length > 0 && id != "new" &&
+            {solidFormViewMetaData?.data?.solidFormViewWorkflowData.length > 0 &&
                 <SolidFormStepper {...props} />
             }
         </div>
