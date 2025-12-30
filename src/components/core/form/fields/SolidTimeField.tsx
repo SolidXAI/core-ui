63d43b6c514228ca8ad326088674af7315a2d15b
Jenendar <jenendar.logicloop@gmail.com>
2025-12-30 14:19:54 +0530

    date field changes

--- src/components/core/form/fields/SolidTimeField.tsx
+++ src/components/core/form/fields/SolidTimeField.tsx
@@ -9,6 +9,7 @@ import { getExtensionComponent } from "@/helpers/registry";
 import { SolidFormFieldWidgetProps } from "@/types/solid-core";
 import { SolidFieldTooltip } from "@/components/common/SolidFieldTooltip";
 import { ERROR_MESSAGES } from "@/constants/error-messages";
+import { DateFieldViewComponent } from "../../common/DateFieldViewComponent";
 
 
 // Converts multiple time formats into a JavaScript Date object
@@ -210,26 +211,36 @@ export const DefaultTimeFormEditWidget = ({ formik, fieldContext }: SolidFormFie
     );
 }
 
-export const DefaultTimeFormViewWidget = ({ formik, fieldContext }: SolidFormFieldWidgetProps) => {
+
+export const DefaultTimeFormViewWidget = ({
+    formik,
+    fieldContext,
+}: SolidFormFieldWidgetProps) => {
     const fieldMetadata = fieldContext.fieldMetadata;
     const fieldLayoutInfo = fieldContext.field;
-    const fieldLabel = fieldLayoutInfo.attrs.label ?? fieldMetadata.displayName;
-    const fieldValue = formik.values[fieldLayoutInfo.attrs.name];
+
+    const fieldName = fieldLayoutInfo.attrs.name;
+    const fieldLabel =
+        fieldLayoutInfo.attrs.label ?? fieldMetadata.displayName;
     const showFieldLabel = fieldLayoutInfo?.attrs?.showLabel;
-    let displayValue = "";
-    if (typeof fieldValue === "string") {
-        const d = parseTimeStringToDate(fieldValue);
-        displayValue = d ? formatTime(d) : "-";
-    } else if (fieldValue instanceof Date) {
-        displayValue = formatTime(fieldValue);
-    }
+
+    const rawValue = formik.values[fieldName];
+    const format = fieldLayoutInfo.attrs?.format
+
 
     return (
-        <div className="mt-2 flex-column gap-2">
+        <div className="mt-2 flex flex-column gap-2">
             {showFieldLabel !== false && (
-                <p className="m-0 form-field-label font-medium">{fieldLabel}</p>
+                <p className="m-0 form-field-label font-medium">
+                    {fieldLabel}
+                </p>
             )}
-            <p className="m-0">{displayValue || "-"}</p>
+
+            <p className="m-0">
+                {/* {displayValue ?? "-"} */}
+                <DateFieldViewComponent value={rawValue} format={format} fallback="-"></DateFieldViewComponent>
+
+            </p>
         </div>
     );
 };
--- src/components/core/list/SolidTableRowCell.tsx
+++ src/components/core/list/SolidTableRowCell.tsx
@@ -1,18 +1,29 @@
+import { Tooltip } from "primereact/tooltip";
+
 const SolidTableRowCell = ({ value, truncateAfter }: { value: string; truncateAfter?: number }) => {
     // Utility function to detect if value contains HTML
     const isHTML = (str: string) => /<\/?[a-z][\s\S]*>/i.test(str);
 
     // Utility function to strip HTML tags
     const stripHTML = (str: string) => str.replace(/<[^>]+>/g, '');
-    
+
     const displayValue = isHTML(value) ? stripHTML(value) : value;
 
     return (
         <div
             className="solid-table-row"
-            style={{ maxWidth: truncateAfter ? `${truncateAfter}ch` : '30ch' }}
+            style={truncateAfter ? { maxWidth: `${truncateAfter}ch` } : {}}
+            // title={truncateAfter ? displayValue : undefined}
         >
             {displayValue}
+            {truncateAfter &&
+                <>
+                    <Tooltip target=".solid-field-tooltip-icon" />
+                    <i className="pi pi-info-circle solid-field-tooltip-icon"
+                        data-pr-tooltip={displayValue}
+                    />
+                </>
+            }
         </div>
     );
 };
