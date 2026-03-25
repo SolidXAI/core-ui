aa26ec1b63cee0c9647bb14ce22da98b4b2ce675
abhishek-pandit <abhishek.pandit@git.com>
2026-03-25 14:27:26 +0530

    feat: format date in SolidChatterDateDivider to a human-readable string.

--- src/components/core/chatter/SolidChatterDateDivider.tsx
+++ src/components/core/chatter/SolidChatterDateDivider.tsx
@@ -9,7 +9,7 @@ export const SolidChatterDateDivider = ({ date }: Props) => {
     return (
         <div className={styles.chatterDatetimeDivider}>
             <div className={`${styles.chatterDividerContent} absolute`}>
-                {date}
+                {new Date(date).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
             </div>
         </div>
     )
