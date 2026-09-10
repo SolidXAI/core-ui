043f0afb60e62c81fb567b72c0e950a9197a31fc
Gaurav Dafale <gaurav.dafale@solidxai.com>
2026-09-10 17:09:04 +0530

    fix: resolve user name display issue

--- src/components/common/SolidThemeLink.tsx
+++ src/components/common/SolidThemeLink.tsx
@@ -1,6 +1,9 @@
 
 export const SolidThemeLink = () => {
     return (
-        <link id="theme-css" href={`/themes/solid-light-purple/theme.css`} rel="stylesheet"></link>
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link id="theme-css" href={`/themes/solid-light-purple/theme.css`} rel="stylesheet"></link>
+        </>
     )
 }
